b0fb8ada46983c1ddd4102801804877db029a1e7
Joseado381 <josealonzo.jaa@gmail.com>
2025-06-12 21:25:15 -0300

    commit sin nombre desde notebook

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -1,1 +1,33 @@
 ﻿
+
+using System;
+using System.IO;
+string path;
+bool existe = false;
+
+do
+{
+    Console.WriteLine("Ingrese el path del directorio:");
+    path = Console.ReadLine();
+    existe = Directory.Exists(path);
+
+    if (!existe)
+    {
+        Console.WriteLine("El directorio no existe. Por favor, ingrese un path válido.");
+    }
+
+} while (!existe);
+
+Console.WriteLine($"\n📁 Directorio: {path}");
+
+string[] directorios = Directory.GetDirectories(path);
+
+foreach (var directorio in directorios)
+{
+    System.Console.WriteLine();
+    System.Console.WriteLine($"directorio: {directorio}");
+    System.Console.WriteLine("---------------------------");
+
+}
+
+Console.WriteLine("Ingrese el nombre del directorio a listar:");
